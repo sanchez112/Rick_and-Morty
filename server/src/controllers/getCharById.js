f44a3ba6f65e8818ec4f13471fcaccdd098ce44d
sanchez <juandavids282005@gmail.com>
2023-12-18 00:47:30 -0500

    19

--- server/src/controllers/getCharById.js
+++ server/src/controllers/getCharById.js
@@ -1,34 +1,28 @@
 const axios = require("axios");
-const URL = "https://rym2.up.railway.app/api/character";
+const { response } = require("express");
+const URL = "https://rickandmortyapi.com/api/character";
 const API_KEY = "henrystaff";
 
+const getCharById = (req, res )=>{
+ const {id} = req.params
+    axios
+        .get(`${URL}/${id}`)
+        .the(({data}) =>{
+            const {
+                id, status, name, species, origin, image, gender, location
 
-const getCharById = (res,id) => {
-    axios.get(`${URL}/${id}?key=${API_KEY}`)
-    .then(response => response.data)
-    .then(data =>{
-        const character ={
-            id:data.id,
-            name: data.name,
-            gender: data.gender,
-            species: data.species,
-            origin: data.origin,
-            image: data.image,
-            status: data.status,
-            location: data.location,
-        };
-        return res
-        .writeHead(200,{"Content-Type":
-        "application/json"})
-        .end(JSON.stringify(character));
-    })
-    .catch(error =>{
-        return res
-        .writeHead(500,{"Content-Type":
-        "application/json"})
-        .end(error.message)
+            }= data;
+            const character = {
+                id, status, name, species, origin, image, gender, location
+            };
+            return character.name
+            ?req.json(character)
+            :req.status(404).send("Not fount")
         })
+        .cat(error => {
+            return res.status(500).send(error.message)
+        });
 
-
+ 
 }
-module.exports =getCharById;
+module.exports = getCharById
--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,9 @@
-const http = require("http");
-const PORT = 3001
-const charaters = require("./utils/data");
-const getCharById = require("./controllers/getCharById");
+const express = require('express');
+const getCharById = require('./controllers/getCharById');
+const server = express();
+const PORT = 3001;
+server.get("/rickandmorty/character/:id", getCharById)
 
-http.createServer((req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    if(req.url.includes("/rickandmorty/character")){
-        const id = req.url.split("/").pop();
-        getCharById(res,id);
-    }
-        
-        
-        
-})
-.listen(PORT, "127.0.0.1", () => (console.log(`server listenig on port ${PORT}`)))
+server.listen(PORT, () => {
+   console.log('Server raised in port: ' + PORT);
+});
